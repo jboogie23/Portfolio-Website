9b375bf1a2834670554096d686eb41f18c27799b
Ross Sanderson <ross.h.sanderson@gmail.com>
2023-09-09 18:16:03 +1000

    no known changes

--- portfolio draft/js/commands.js
+++ portfolio draft/js/commands.js
@@ -1,27 +1,15 @@
-var youtube = "https://www.youtube.com/fknight/";
-var twitter = "https://www.twitter.com/forrestpknight/";
+var youtube = "https://www.youtube.com/";
+var twitter = "https://www.twitter.com/";
 var password = "fkcodes";
-var linkedin = "https://www.linkedin.com/in/forrestpknight/";
-var instagram = "https://www.instagram.com/forrestpknight/";
-var github = "https://github.com/forrestknight/";
-var email = 'mailto:forrest@fkcodes.com';
+var linkedin = "https://www.linkedin.com/";
+var instagram = "https://www.instagram.com/";
+var github = "https://github.com/";
+var email = 'mailto:ross.h.sanderson@gmail.com';
 
 whois = [
   "<br>",
-  "Hey, I'm Forrest!ðŸ‘‹",
-  "I'm a software developer and content creator, who builds engaging websites like this one",
-  "and makes YouTube videos about computer science & software engineering.",
-  "After graduating with a Bachelor's in Computer Science, I worked professionally",
-  "as a software engineer building enterprise web applications for Fortune 500 companies.",
-  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
-  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
-  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
-  "I needed to make the jump from software engineering to full time content creator, and it's",
-  "the best decision I ever made.",
-  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
-  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
-  "because they're reading this right now, is that I also run a creative & media agency.",
-  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "Hey, I'm Ross!ðŸ‘‹",
+  "I'm going to fill this out later",
   "<br>"
 ];
 
@@ -33,11 +21,11 @@ whoami = [
 
 social = [
   "<br>",
-  'youtube        <a href="' + youtube + '" target="_blank">youtube/fknight' + "</a>",
-  'twitter        <a href="' + twitter + '" target="_blank">twitter/forrestpknight' + '</a>',
-  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/forrestpknight' + "</a>",
-  'instagram      <a href="' + instagram + '" target="_blank">instagram/forrestpknight' + '</a>',
-  'github         <a href="' + github + '" target="_blank">github/forrestknight' + "</a>",
+  'youtube        <a href="' + youtube + '" target="_blank">youtube/' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/' + "</a>",
   "<br>"
 ];
 
@@ -55,7 +43,7 @@ projects = [
 
 help = [
   "<br>",
-  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whois</span>          Who is Ross?',
   '<span class="command">whoami</span>         Who are you?',
   '<span class="command">video</span>          View YouTube videos',
   '<span class="command">social</span>         Display social networks',
@@ -73,13 +61,7 @@ banner = [
   '<span class="index">ForrestKnight (FK) Not A Corporation. All knights reserved.</span>',
   "         ___   ____",
   "       /' --;^/ ,-_\\     \\ | /       ",
-  "      / / --o\\ o-\\ \\\\   --(_)--   ",
-  "     /-/-/|o|-|\\-\\\\|\\\\   / | \\   ______                          __     __ __       _       __    __   ",
-  "      '`  ` |-|   `` '          / ____/___  _____________  _____/ /_   / //_/____  (_)___ _/ /_  / /_ ",
-  "            |-|                / /_  / __ \\/ ___/ ___/ _ \\/ ___/ __/  / ,<  / __ \\/ / __ `/ __ \\/ __/ ",
-  "            |-|O              / __/ / /_/ / /  / /  /  __(__  ) /_   / /| |/ / / / / /_/ / / / / /_  ",
-  "            |-(\\,__          /_/    \\____/_/  /_/   \\___/____/\\__/  /_/ |_/_/ /_/_/\\__, /_/ /_/\\__/ ",
-  "         ...|-|\\--,\\_....                                                         /____/    Â© 2022",
+  "      / / --o\\ o-\\ \\\\   --(_)--   ACSCI goes here",
   "      ,;;;;;;;;;;;;;;;;;;;;;;;;,.   ",
   "~~,;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;,~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
   '<span class="color2">Welcome to my interactive web terminal.</span>',
